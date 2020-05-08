08abf4d679d8adfc343157f428000b1282f1fe94
21ric <55327282+21ric@users.noreply.github.com>
2020-05-08 16:02:13 +0200

    Update alexnet_custom.py

--- alexnet_custom.py
+++ alexnet_custom.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
-from torch.utils import load_state_dict_from_url
-
+#from torch.utils import load_state_dict_from_url
+from torch.hub import load_state_dict_from_url
 
 __all__ = ['AlexNet', 'alexnet']
 
